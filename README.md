90cd0560c27e6a09f568a326179590ac3d7c9e0a
Unknown <elendil_le_grand@hotmail.com>
2018-06-02 11:16:08 +0200

    Ajout generate class

--- README.md
+++ README.md
@@ -10,9 +10,9 @@
 - Fichiers d'entrée (main.tsx et electronStart.ts), tsconfig préconfigurés et structure www créés via l'initialisation
 - Utilisation de jsx via l'import d'un fichier jsx.ts
 - Divers fonctions disponibles grâce à l'import du fichier dom.ts
+- Créer vos components via une ligne de commande (pour une bonne intégration dans tsconfig.json et faciliter les imports)
 
 A venir :
-- Créer vos components via une ligne de commande (pour une bonne intégration dans tsconfig.json et faciliter les imports)
 - Pré-intégration d'un service-worker
 
 ## Install
@@ -24,7 +24,7 @@ A venir :
 - npm install
 - Créer un dossier src
 - npx wapitis init
-- Modifier tsconfig pour intégrer les fichiers ts
+- Utiliser wapitis **$ generate class path/du/fichier.ts(x)** pour générer vos fichiers
 - Intégrer vos icons dans le fichiers icons.svg (exemple à intégrer en image)
 - Intégrer vos css dans le fichier main.css
 - Le point de départ est le dossier main.ts
@@ -41,15 +41,17 @@ A venir :
 
 **$ npx wapitis** pour obtenir l'aide :
 
-    $ npx wapitis init     --> initialise la web app en créant les fichiers et les dossiers nécessaires
+    $ npx wapitis init --> initialise la web app en créant les fichiers et les dossiers nécessaires
 
-    $ npx wapitis dev      --> lance la web app dans un serveur local
+    $ npx wapitis dev --> lance la web app dans un serveur local
 
-    $ npx wapitis prod     --> web app pour la production
+    $ npx wapitis prod --> web app pour la production
 
-    $ npx wapitis electron --> lance la web app dans electron avec un serveur local (--dev) ou pour la production(--prod)
+    $ npx wapitis electron --> lance la webApp dans electron avec un serveur local (--dev) ou pour la production(--prod)
 
-    $ npx wapitis clear    --> supprime le cache et le dossier dist
+    $ npx wapitis clear --> supprime le cache et le dossier dist
+
+    $ wapitis generate class path/du/fichier.ts(x) --> génère un fichier relatif à src. tsConfig est mis à jour
 
 ### Typescript
 
--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const { FuseBox, QuantumPlugin, CSSPlugin, CSSResourcePlugin, WebIndexPlugin, Co
 const { src, task, exec, context } = require("fuse-box/sparky");
 const builder = require("electron-builder");
 
+// PACKAGE JSON
+const packageJson = JSON.parse(files.readFileSync(directoryBase + "/package.json", "utf8"));
+
 function formatDateToYYYYMMDDHHMM(date) {
     function pad2(n) {  // always returns a string
         return (n < 10 ? '0' : '') + n;
@@ -40,34 +43,27 @@ if (arg) {
 					else return 'Entrez le nom de votre dossier source';
 				},
 				default: "src"
-			},
-			{
-				name: 'installdefaultfiles',
-				type: 'confirm',
-				message: 'Voulez-vous installer les fichiers et dossiers par défaut (dossier www, fichiers app.tsx, ...):'
 			}
 		];
 		  
 		inquirer.prompt(questions).then((answers) => {
-			const wapitisTxt = `{
-	"srcPath": "` + answers.srcproject + `/",
-	"wwwPath": "` + answers.srcproject + `/www",
-	"distPath": "` + answers.srcproject + `/../dist",
+			const wapitisTxt = 
+`{
+	"srcPath": "${answers.srcproject}/",
+	"wwwPath": "${answers.srcproject}/www",
+	"distPath": "${answers.srcproject}/../dist",
 	"startFile": "app.tsx",
 	"electronStartFile": "electronStart.ts"
 }`;		
-			files.appendFile(process.cwd() + "/wapitis.json", wapitisTxt, true);
-			if (answers.installdefaultfiles) {
-				files.copy(path.resolve(__dirname, "files/tsconfig.json"), process.cwd() + "/tsconfig.json");
-				files.copy(path.resolve(__dirname, "files/app.tsx"), process.cwd() + "/" + answers.srcproject + "/app.tsx");
-				files.copy(path.resolve(__dirname, "files/electronStart.ts"), process.cwd() + "/" + answers.srcproject + "/electronStart.ts");
-				files.copy(path.resolve(__dirname, "files/www"), process.cwd() + "/" + answers.srcproject + "/www");
-			}
-			const packageJson = JSON.parse(files.readFileSync(process.cwd() + "/package.json", "utf8"));
+			files.appendFile(directoryBase + "/wapitis.json", wapitisTxt, true);
+			files.copy(path.resolve(__dirname, "files/tsconfig.json"), directoryBase + "/tsconfig.json");
+			files.copy(path.resolve(__dirname, "files/app.tsx"), directoryBase + "/" + answers.srcproject + "/app.tsx");
+			files.copy(path.resolve(__dirname, "files/electronStart.ts"), directoryBase + "/" + answers.srcproject + "/electronStart.ts");
+			files.copy(path.resolve(__dirname, "files/www"), directoryBase + "/" + answers.srcproject + "/www");
 			packageJson.main = answers.srcproject + "/../dist/electron.js";
-			files.appendFile(process.cwd() + "/package.json", JSON.stringify(packageJson, null, 2), true);
+			files.appendFile(directoryBase + "/package.json", JSON.stringify(packageJson, null, 2), true);
 		});
-	} else if (arg === "dev" || arg === "prod" || arg == "clear" || arg == "electron") {
+	} else if (arg === "dev" || arg === "prod" || arg === "clear" || arg === "electron" || arg === "generate") {
 
 		// GLOBALS
 		const isElectron = arg === "electron";
@@ -77,12 +73,46 @@ if (arg) {
 		// wapitis CONFIG
 		const wapitisConfig = JSON.parse(files.readFileSync(directoryBase + "/wapitis.json", "utf8"));
 
-		// PACKAGE JSON
-		const packageJson = JSON.parse(files.readFileSync(directoryBase + "/package.json", "utf8"));
+		// Génération de fichiers
+		if (arg === "generate") {
+			const arg2 = process.argv[3];
+			const arg3 = process.argv[4];
+			if (process.argv[3] === "class" && (arg3.includes(".ts") || arg3.includes(".tsx"))) {
+				log(chalk.green("wapitis generate " + arg2 + " " + wapitisConfig.srcPath + arg3));
+				const classFile = wapitisConfig.srcPath + arg3;
+				if (!files.fileExists(directoryBase + "/" + classFile)) {
+					let className = arg3.substr(arg3.lastIndexOf("/") + 1);
+					className = className.substr(0, className.lastIndexOf("."));
+					className = className.split(/[- _]+/).map((str) => str.charAt(0).toUpperCase() + str.substr(1)).join("");
+					const classText = 
+(arg3.includes(".tsx") ? 
+`import { JSX } from "wapitis";
+
+` : "") +
+`export default class ${className} {
+
+}
+`;
+					files.appendFile(directoryBase + "/" + classFile, classText, true);
+					const tsConfigJson = JSON.parse(files.readFileSync(tsconfigFile, "utf8"));
+					const tsConfigJsonPath = "*" in tsConfigJson.compilerOptions.paths ? tsConfigJson.compilerOptions.paths["*"] : tsConfigJson.compilerOptions.paths["*"] = [];
+					const tsConfigClassPath = classFile.substr(0, classFile.lastIndexOf("/") + 1) + "*";
+					if (!tsConfigJsonPath.includes(tsConfigClassPath)) {
+						tsConfigJson.compilerOptions.paths["*"].push(tsConfigClassPath);
+						files.appendFile(directoryBase + "/tsConfig.json", JSON.stringify(tsConfigJson, null, 2), true);
+					}
+					log(chalk.green("Le fichier " + classFile + " a été créé"));
+				} else log(chalk.red("Impossible de générer " + classFile + " : le fichier existe déjà"));
+				
+			} else log(chalk.red("Cette commande n'est pas pris en charge par wapitis generate"));
+			return;
+		}
 
 		// Combine custom icons SVG in dom icons SVG
-		const customSVG = files.readFileSync(wapitisConfig.wwwPath + "/assets/img/icons.svg", "utf8");
-		files.appendFile(path.resolve(__dirname, "library/icons.svg"), customSVG, true);
+		if (arg !== "clear") {
+			const customSVG = files.readFileSync(wapitisConfig.wwwPath + "/assets/img/icons.svg", "utf8");
+			files.appendFile(path.resolve(__dirname, "library/icons.svg"), customSVG, true);
+		}
 
 		// PATH IMPORT ALIAS
 		let importFiles = {};
@@ -218,7 +248,7 @@ if (arg) {
 						let fileName = result[1].substring(result[1].lastIndexOf(spacer)+1);
 						files.copy(result[1], directoryBase + "/" + wapitisConfig.distPath + "/" + packageJson.name + "_" + packageJson.version + "_" + formatDateToYYYYMMDDHHMM(new Date()) + "_setup" + fileName.substring(fileName.lastIndexOf("."))).then(() => files.remove(__dirname + "/dist"));
 					})
-					.catch((error) => console.log(error));
+					.catch((error) => log(error));
 				});
 			} else {
 				await fuse.run().then(() => {
@@ -229,7 +259,7 @@ if (arg) {
 		});
 
 	} else {
-		log(chalk.green.bold(arg) + " n'est pas pris en charge par wapitis")
+		log(chalk.red(arg + " n'est pas pris en charge par wapitis"));
 	}
 } else {
 	log(chalk.green(figlet.textSync('WApiTis', { horizontalLayout: 'full' }) + chalk.bold(' ' + JSON.parse(files.readFileSync(__dirname + "/package.json", "utf8")).version)));
@@ -237,6 +267,7 @@ if (arg) {
 	log(chalk.green(chalk.bold("  wapitis init") + " ---> initialise la web app en créant les fichiers et les dossiers nécessaires"));
 	log(chalk.green(chalk.bold("  wapitis dev") + "  ---> lance la web app dans un serveur local"));
 	log(chalk.green(chalk.bold("  wapitis prod") + " ---> web app pour la production"));
-	log(chalk.green(chalk.bold("  wapitis electron") + "  ---> lance la web app dans electron avec un serveur local (--dev) ou pour la production(--prod)"));
+	log(chalk.green(chalk.bold("  wapitis electron") + "  ---> lance la webApp dans electron avec un serveur local (--dev) ou pour la production(--prod)"));
 	log(chalk.green(chalk.bold("  wapitis clear") + " ---> supprime le cache et le dossier dist"));
+	log(chalk.green(chalk.bold("  wapitis generate class path/du/fichier.ts(x)") + " ---> génère un fichier relatif à src. tsConfig est mis à jour"));
 }
