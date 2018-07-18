9f45606878038ea827a0d7428d22f89c803efae2
Nicolas <elendil_le_grand@hotmail.com>
2018-07-18 15:12:36 +0200

    release: v1.0.4 - reorg

--- transpile.js
+++ transpile.js
@@ -2,8 +2,8 @@
 
 "use strict";
 
-const files = require("./helpers/files");
-const tools = require("./helpers/tools");
+const files = require("./tools/files");
+const tools = require("./tools/tools");
 
 // Permet de créer la librairie avec l'aide du tsconfig et de ce qui est dans src (avec namespace et export) dans le dossier library
 files.remove("./library");
