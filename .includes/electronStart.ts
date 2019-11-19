bf650531c3a536316a9ca8a38af536fca02e9e4f
Nicolas <elendil_le_grand@hotmail.com>
2019-11-19 19:14:42 +0100

    feat: splash

--- .includes/electronStart.ts
+++ .includes/electronStart.ts
@@ -5,35 +5,46 @@ import * as url from 'url'
 const ENV = process.env.NODE_ENV || 'production'
 const DEBUG = ENV === 'development'
 let win: any
+let splash: any
 
 function createWindow() {
-  win = new BrowserWindow()
-  win.maximize()
+    win = new BrowserWindow({ show: false })
+    splash = new BrowserWindow({ width: 810, height: 610, transparent: true, frame: false, alwaysOnTop: true })
 
-  if (DEBUG) {
-    win.loadURL('http://localhost:4444')
-    // Open the DevTools.
-    win.webContents.openDevTools()
-  } else {
-    win.loadURL(url.format({
-      pathname: path.join(app.getAppPath(), 'dist', 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
-  }
-  win.on('closed', () => win = null)
+    if (DEBUG) {
+        splash.loadURL('http://localhost:4444/splash.html')
+        win.loadURL('http://localhost:4444')
+        // Open the DevTools.
+        win.webContents.openDevTools()
+    } else {
+        splash.loadURL(url.format({
+            pathname: path.join(app.getAppPath(), 'dist', 'splash.html'),
+            protocol: 'file:',
+            slashes: true
+        }))
+        win.loadURL(url.format({
+            pathname: path.join(app.getAppPath(), 'dist', 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        }))
+    }
+    win.once('ready-to-show', () => {
+        splash.destroy()
+        win.maximize()
+    })
+    win.on('closed', () => win = null)
 }
 
 app.on('ready', () => createWindow())
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+    if (win === null) {
+        createWindow()
+    }
 })
