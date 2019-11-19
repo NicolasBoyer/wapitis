bce2c08a5e240f1bbe0d3d0a63bd1864caa41bec
Nicolas <elendil_le_grand@hotmail.com>
2019-11-19 19:21:54 +0100

    refactor + feat: async et service worker

--- tools/swbuilder.js
+++ tools/swbuilder.js
@@ -1,6 +1,29 @@
 const files = require("./files");
 const path = require('path');
+const webpush = require('web-push');
 let globDirectory, swDest, excludeFiles, patterns, wapitisConfig;
+let sw = `const DEBUG = false;
+
+/**
+ * log si debug à true
+ */
+function log(message, ...data) {
+    if (DEBUG) {
+        if (data.length > 0) console.log(DATE, message, data);
+        else console.log(DATE, message);
+    }
+}
+
+/**
+ * Affiche une notification locale simple
+ */
+function showNotification(title, datas = {}) {
+    if (Notification.permission === "granted") {
+        datas.icon = "./assets/icons/icons-192.png";
+        self.registration.showNotification(title, datas);
+    }
+}
+`;
 
 const swbuilder = module.exports = {
     setOptions: (options) => {
@@ -11,31 +34,27 @@ const swbuilder = module.exports = {
         patterns = options.patterns;
     },
 
-    registerServiceWorker: () => {
+    registerServiceWorker: async () => {
         wapitisConfig = JSON.parse(files.readFileSync(globDirectory + "/wapitis.json", "utf8"));
         const cacheUrls = {}
         wapitisConfig.cacheVersion = wapitisConfig.cacheVersion || {}
-        // TODO async ?
-        return new Promise((resolve) => {
-            Object.keys(patterns).forEach((name) => {
-                const pattern = patterns[name]
-                let isCacheVersionRegister = false
-                files.getAllFiles(swDest, excludeFiles).allFiles.forEach((file) => {
-                    if (!cacheUrls[name]) cacheUrls[name] = name === 'core' ? ["./"] : []
-                    if (path.extname(file).toLowerCase().match(pattern)) {
-                        cacheUrls[name].push(file.substring(swDest.length).split("\\").join("/"))
-                        const fileName = path.basename(file).split('-')[1] || path.basename(file)
-                        if ((wapitisConfig.filesInfos.update.includes(fileName) || name === 'core') && !isCacheVersionRegister) {
-                            wapitisConfig.cacheVersion[name] = (wapitisConfig.cacheVersion[name] || 0) + 1
-                            isCacheVersionRegister = true
-                        }
+        Object.keys(patterns).forEach((name) => {
+            const pattern = patterns[name]
+            let isCacheVersionRegister = false
+            files.getAllFiles(swDest, excludeFiles).allFiles.forEach((file) => {
+                if (!cacheUrls[name]) cacheUrls[name] = name === 'core' ? ["./"] : []
+                if (path.extname(file).toLowerCase().match(pattern)) {
+                    cacheUrls[name].push(file.substring(swDest.length).split("\\").join("/"))
+                    const fileName = path.basename(file).split('-')[1] || path.basename(file)
+                    if ((wapitisConfig.filesInfos.update.includes(fileName) || name === 'core') && !isCacheVersionRegister) {
+                        wapitisConfig.cacheVersion[name] = (wapitisConfig.cacheVersion[name] || 0) + 1
+                        isCacheVersionRegister = true
                     }
-                    // TODO une question se pose quand c fait en html - comment faire fonctionner ça dans ce cas et le prendre en compte
-                })
+                }
+                // TODO une question se pose quand c fait en html - comment faire fonctionner ça dans ce cas et le prendre en compte
             })
-            let sw = `
-const DEBUG = false;
-
+        })
+        sw += `
 const RUNTIME = 'runtime';
 const DATE = '$date$';
 const CACHES_VERSION = $caches_version$;
@@ -102,88 +121,145 @@ self.addEventListener('push', (event) => {
     };
     return showNotification(dataJSON.title, notificationOptions);
 });
+
 self.addEventListener('notificationclick', (event) => {
     const url =  event.notification.data && event.notification.data.url;
     if (url) {
         event.notification.close();
         event.waitUntil(clients.openWindow(url));
     }
-});
-
-
-/**
- * log si debug à true
- */
-function log(message, ...data) {
-    if (DEBUG) {
-        if (data.length > 0) console.log(DATE, message, data);
-        else console.log(DATE, message);
-    }
-}
-
-/**
- * Affiche une notification locale simple
- */
-function showNotification(title, datas = {}) {
-    if (Notification.permission === "granted") {
-        // datas.icon = $icon$;
-        self.registration.showNotification(title, datas);
-    }
-}`;
-            sw = sw.replace("$current_caches_urls$", JSON.stringify(cacheUrls))
-            sw = sw.replace("$caches_version$", JSON.stringify(wapitisConfig.cacheVersion))
-            sw = sw.replace("$date$", new Date().toLocaleString())
-            files.appendFile(globDirectory + "/wapitis.json", JSON.stringify(wapitisConfig, null, 2), true);
-            files.appendFile(swDest + "/sw.js", sw, true);
-            // TODO ICI - reste à créer un bouton pour activer les notif + use node pour créer la key + icon dans sw notif
-            files.readFile(indexSrc + "/index.html", (err, html) => {
-                if (err) throw err;
-                html = html.replace("$headScripts$", '<link rel="manifest" href="manifest.json"/><script src="polyfills.js"></script>');
-                html = html.replace("$bodyScript$", `
-<script>
-const apiKey = null; // Clé à générer ? marche avec push
-function urlB64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/');
-
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-}
-
-async function pushSubscribe(swRegistration) {
-    if (apiKey && 'PushManager' in window) {
-        const pushManager = swRegistration.pushManager;
-        let subscription;
+});`;
+        sw = sw.replace("$current_caches_urls$", JSON.stringify(cacheUrls))
+        sw = sw.replace("$caches_version$", JSON.stringify(wapitisConfig.cacheVersion))
+        sw = sw.replace("$date$", new Date().toLocaleString())
+        await files.appendFile(globDirectory + "/wapitis.json", JSON.stringify(wapitisConfig, null, 2), true);
+        await files.appendFile(swDest + "/sw.js", sw, true);
         try {
-            subscription = await pushManager.subscribe({applicationServerKey: urlB64ToUint8Array(apiKey), userVisibleOnly: true});
-            console.log("Clé de souscription :");
-            console.log(JSON.stringify(subscription));
-        } catch (error) {
-            const oldSubscription = await pushManager.getSubscription();
-            await oldSubscription.unsubscribe();
-            pushSubscribe(swRegistration);
-        }
-    }
-}
+            let html = await files.readFile(indexSrc + "/index.html")
+            html = html.replace("$headScripts$", '<link rel="manifest" href="manifest.json"/><script src="polyfills.js"></script>');
+            html = html.replace("$bodyScript$", `
+<script>
+// const apiKey = "` + webpush.generateVAPIDKeys().publicKey + `";
+// function urlB64ToUint8Array(base64String) {
+//     const padding = '='.repeat((4 - base64String.length % 4) % 4);
+//     const base64 = (base64String + padding)
+//         .replace(/\-/g, '+')
+//         .replace(/_/g, '/');
+
+//     const rawData = window.atob(base64);
+//     const outputArray = new Uint8Array(rawData.length);
+
+//     for (let i = 0; i < rawData.length; ++i) {
+//         outputArray[i] = rawData.charCodeAt(i);
+//     }
+//     return outputArray;
+// }
+
+// async function pushSubscribe(swRegistration) {
+//     if (apiKey && 'PushManager' in window) {
+//         const pushManager = swRegistration.pushManager;
+//         const subscription = {endpoint: ''};
+//         try {
+//             subscription = await pushManager.subscribe({applicationServerKey: urlB64ToUint8Array(apiKey), userVisibleOnly: true});
+//             console.log("Clé de souscription :");
+//             console.log(JSON.stringify(subscription));
+//             createPushNotificationButton(subscription);
+//         } catch (error) {
+//             const oldSubscription = await pushManager.getSubscription();
+//             await oldSubscription.unsubscribe();
+//             pushSubscribe(swRegistration);
+//         }
+//     }
+// }
 
 async function registerSW() {
     await navigator.serviceWorker.register("sw.js");
     const swRegistration = await navigator.serviceWorker.ready;
-    pushSubscribe(swRegistration);
+    createPermissionNotificationButton(swRegistration)
 }
 
+function createPermissionNotificationButton(swRegistration) {
+    if ("Notification" in window && (Notification.permission !== 'denied' || Notification.permission === "default") && Notification.permission !== "granted") {
+        var buttonParent = document.querySelector(".notificationPermission"),
+            button;
+        if (buttonParent) {
+            button = buttonParent.querySelector("button");
+        } else {
+            document.body.classList.add('notificationbutton_visible_true');
+            buttonParent = document.createElement("div");
+            buttonParent.className = "notificationPermission";
+            button = document.createElement("button");
+            button.title = "Afficher les notifications";
+            button.innerHTML = "<span>Afficher les notifications</span>";
+            buttonParent.appendChild(button);
+            document.body.insertBefore(buttonParent, document.body.firstChild);
+        }
+        button.addEventListener('click', () => {
+            button.style.display = "none";
+            Notification.requestPermission((permission) => {
+                if (permission === "granted") {
+                    // pushSubscribe(swRegistration);
+                    swRegistration.showNotification('Notifications activées', { body: 'Merci d\\'avoir activées les notifications.', icon: './assets/icons/icons-192.png' });
+                }
+            });
+        });
+    }
+}
+
+// TODO a priori il faut récupérer toutes les subscriptions pour permettre l'envoie des notifications via ces souscriptions -> A faire plus tard. Pour l'instant je ne garde que les local notifications
+// async function sendNotification(subscription) {
+//     webpush.sendNotification(subscription)
+//     console.log("blip")
+// }
+
+// function createPushNotificationButton(subscription) {
+//     if ("Notification" in window) {
+//         var buttonParent = document.querySelector(".pushNotification"),
+//             button;
+//         if (buttonParent) {
+//             button = buttonParent.querySelector("button");
+//         } else {
+//             document.body.classList.add('notificationbutton_visible_true');
+//             buttonParent = document.createElement("div");
+//             buttonParent.className = "pushNotification";
+//             button = document.createElement("button");
+//             button.title = "Envoyer une notification";
+//             button.innerHTML = "<span>Envoyer une notification</span>";
+//             buttonParent.appendChild(button);
+//             document.body.insertBefore(buttonParent, document.body.firstChild);
+//         }
+//         button.addEventListener('click', () => {
+//             var win  = document.createElement("div");
+//             win.className = "pushNotificationWin";
+//             var winTitle = document.createElement("div");
+//             winTitle.className = "title";
+//             winTitle.innerHTML = "<span>Envoyer une notification</span>";
+//             var form = document.createElement("form");
+//             form.innerHTML = \`
+//             <fieldset>
+//                 <legend>Votre identité</legend>
+//                 <label for="title">
+//                     <span>Titre</span>
+//                     <input id="title" name="title" type="text" placeholder="Titre de la notification" required>
+//                 </label>
+//                 <label for="url">
+//                     <span>Url</span>
+//                     <input id="url" name="url" type="text" placeholder="Url">
+//                 </label>
+//                 <label for="message">
+//                     <span>Message</span>
+//                     <textarea id="message" name="message" required></textarea>
+//                 </label>
+//             </fieldset>
+//             <button type="button" onClick="sendNotification(subscription)">Envoyer</button>\`;
+//             win.appendChild(winTitle);
+//             win.appendChild(form);
+//             document.body.appendChild(win);
+//         });
+//     }
+// }
+
 if ('serviceWorker' in navigator) {
-    // Notification.requestPermission((permission) => {
-    //     Notification.permission = permission;
-    //     return permission;
-    // });
     window.addEventListener("load", registerSW());
 } else {
     console.warn('Le navigateur ne prend pas en charge les services workers ou les push manager');
@@ -205,7 +281,6 @@ window.addEventListener('beforeinstallprompt', (event) => {
         // Add class to body
         document.body.classList.add('pwabutton_visible_true');
         buttonParent = document.createElement("div");
-        document.createElement("div");
         buttonParent.className = "installApp";
         button = document.createElement("button");
         button.title = "Ajouter à l'écran d'accueil";
@@ -231,18 +306,16 @@ window.addEventListener('beforeinstallprompt', (event) => {
     });
 });
 </script>
-                `);
-                files.appendFile(swDest + "/index.html", html, true).then(() => resolve());
-            });
-        });
+            `);
+            await files.appendFile(swDest + "/index.html", html, true);
+        } catch (error) {
+            throw error
+        }
     },
 
-    unregisterServiceWorker: () => {
+    unregisterServiceWorker: async () => {
         wapitisConfig = JSON.parse(files.readFileSync(globDirectory + "/wapitis.json", "utf8"));
-        return new Promise((resolve) => {
-            const sw = `
-const DEBUG = false;
-
+        sw += `
 /**
  * Désinstalle le service worker
  */
@@ -255,6 +328,7 @@ async function uninstallSW() {
         return self.skipWaiting();
     });
 }
+
 self.addEventListener('activate', () => desactivateSW());
 async function desactivateSW() {
     log('Service Worker désactivé');
@@ -264,32 +338,12 @@ async function desactivateSW() {
         return self.clients.claim()
     });
     showNotification('Application désactivée et désinstallée', { body: 'L\\'application a été désinstallée.' });
-}
-
-/**
- * log si debug à true
- */
-function log(message, ...data) {
-    if (DEBUG) {
-        if (data.length > 0) console.log(DATE, message, data);
-        else console.log(DATE, message);
-    }
-}
-
-/**
- * Affiche une notification locale simple
- */
-function showNotification(title, datas = {}) {
-    if (Notification.permission === "granted") {
-        // datas.icon = $icon$;
-        self.registration.showNotification(title, datas);
-    }
 }`;
-            files.appendFile(swDest + "/sw.js", sw, true);
-            files.readFile(indexSrc + "/index.html", (err, html) => {
-                if (err) throw err;
-                html = html.replace("$headScripts$", "");
-                html = html.replace("$bodyScript$", `
+        await files.appendFile(swDest + "/sw.js", sw, true);
+        try {
+            let html = await files.readFile(indexSrc + "/index.html")
+            html = html.replace("$headScripts$", "");
+            html = html.replace("$bodyScript$", `
 <script>
 async function requireUnregister() {
     const registration = await navigator.serviceWorker.getRegistrations()
@@ -305,21 +359,18 @@ async function unregisterSW() {
 }
 
 if ('serviceWorker' in navigator && 'PushManager' in window) {
-    // Notification.requestPermission((permission) => {
-    //     Notification.permission = permission;
-    //     return permission;
-    // });
     requireUnregister()
 } else {
     console.warn('Le navigateur ne prend pas en charge les services workers ou les push manager');
 }
 </script>`);
-                files.appendFile(swDest + "/index.html", html, true).then(() => resolve());
-                if (wapitisConfig.filesInfos) {
-                    delete wapitisConfig.filesInfos.update;
-                }
-                files.appendFile(globDirectory + "/wapitis.json", JSON.stringify(wapitisConfig, null, 2), true);
-            });
-        });
+            await files.appendFile(swDest + "/index.html", html, true);
+            if (wapitisConfig.filesInfos) {
+                delete wapitisConfig.filesInfos.update;
+            }
+            await files.appendFile(globDirectory + "/wapitis.json", JSON.stringify(wapitisConfig, null, 2), true);
+        } catch (error) {
+            throw error
+        }
     }
 };
