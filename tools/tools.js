039230aae4c8d683efc953dbb3aef075a531522c
Nicolas <elendil_le_grand@hotmail.com>
2019-12-10 01:13:46 +0100

    feat: exec async

--- tools/tools.js
+++ tools/tools.js
@@ -1,15 +1,20 @@
 const execute = require('child_process').exec;
 
 const tools = module.exports = {
-	runCommand: async (cmd, cwd, noOutput) => {
-		await new Promise(() => {
-			const folderExec = cwd || null
+	runCommand: (cmd, cwd, noOutput) => {
+		const folderExec = cwd || null
+		return new Promise((resolve, reject) => {
 			execute(cmd, { cwd: folderExec }, (error, stdout, stderr) => {
+				if (error) {
+					reject(error);
+					return;
+				}
 				if (!noOutput) {
 					console.log(stdout)
 					console.log(stderr)
 				}
+				resolve(stdout.trim())
 			})
 		})
 	}
-};
+}
